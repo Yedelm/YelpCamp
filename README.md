5d1d72e3a587f8b42a9438368166d4db6ae972e8
Hanwei Liu <lhanwei.1993@gmail.com>
2018-01-23 04:00:24 +0000

    Some basic route and styling

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # My YelpCamp Project
 
 Part 1.
-Add Landing Page
-Add Campgrounds Page that lists all campgrounds: (Structrue)
-    *name(from faker)  *image(from http://photosforclass.com/search?text=camp)
-    [
-        {name: "Hanwei", image = "http://..."}
-        {name: "xxx", image = "http://..."}
-    ]
+    Add Landing Page
+    Add Campgrounds Page that lists all campgrounds: (Structrue)
+        *name(from faker)  
+        *image(from http://photosforclass.com/search?text=camp)
+    Add some styling
+        *partials, header & footer
+        *boostrap include
+    Add POST route
+    Add body-parser
+    Setup route to show form and add basic unstyled form
+    Add better header/title
+    Make campgrounds display in a grid
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var faker = require("faker");
+var bodyParser = require("body-parser");
 var app = express();
 var campgrounds = [
         {name: faker.name.findName(), image:"https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
@@ -8,6 +9,7 @@ var campgrounds = [
     ];
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req, res){
    res.render("landing"); 
@@ -15,6 +17,20 @@ app.get("/",function(req, res){
 
 app.get("/campgrounds", function(req, res){
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds",function(req, res){
+   //get data form form, add to arrary
+   var name = req.body.name;
+   var image = req.body.image;
+   var newCampground = {name: name, image:image};
+   campgrounds.push(newCampground);
+   //redirect back to campground page
+   res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+   res.render("new"); 
 });
 
 app.listen(process.env.PORT, process.env.IP,function(){
