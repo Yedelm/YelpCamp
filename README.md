f8376c309d44e2f5c09e022ab7f6cb93119689ae
Hanwei Liu <lhanwei.1993@gmail.com>
2018-01-25 03:22:55 +0000

    Better header & campgrounds styling

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # My YelpCamp Project
 
 Part 1.
+    #initial setup
     Add Landing Page
     Add Campgrounds Page that lists all campgrounds: (Structrue)
         *name(from faker)  
         *image(from http://photosforclass.com/search?text=camp)
+    
+    #Layout & basic styling
     Add some styling
         *partials, header & footer
         *boostrap include
+   
+    #Creating new campgrounds
     Add POST route
     Add body-parser
     Setup route to show form and add basic unstyled form
-    Add better header/title
+    
+    #styling campgrounds page
+    Add better header/title  (boostrap)
     Make campgrounds display in a grid
+    
+    #style nav bar & form
+    Add nav bar to all pages
+    Style the new campgrounds form
--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var bodyParser = require("body-parser");
 var app = express();
 var campgrounds = [
         {name: faker.name.findName(), image:"https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
+        {name: faker.name.findName(), image:"https://farm2.staticflickr.com/1203/1132895352_afd086a60b.jpg"},
+        {name: faker.name.findName(), image:"https://farm9.staticflickr.com/8302/7820598746_4d8c11899e.jpg"},
+        {name: faker.name.findName(), image:"https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
+        {name: faker.name.findName(), image:"https://farm2.staticflickr.com/1203/1132895352_afd086a60b.jpg"},
+        {name: faker.name.findName(), image:"https://farm9.staticflickr.com/8302/7820598746_4d8c11899e.jpg"},
+        {name: faker.name.findName(), image:"https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
+        {name: faker.name.findName(), image:"https://farm2.staticflickr.com/1203/1132895352_afd086a60b.jpg"},
+        {name: faker.name.findName(), image:"https://farm9.staticflickr.com/8302/7820598746_4d8c11899e.jpg"},
+        {name: faker.name.findName(), image:"https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
+        {name: faker.name.findName(), image:"https://farm9.staticflickr.com/8302/7820598746_4d8c11899e.jpg"},
+        {name: faker.name.findName(), image:"https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
         {name: faker.name.findName(), image:"https://farm9.staticflickr.com/8302/7820598746_4d8c11899e.jpg"},
         {name: faker.name.findName(), image:"https://farm2.staticflickr.com/1203/1132895352_afd086a60b.jpg"}
     ];
